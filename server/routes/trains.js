ee1784c90074efab79cc2f9e58c4f8b49928f169
marco de mojà <marco de moj>
2017-11-26 17:03:33 +0000

    check irish rail api response

--- server/routes/trains.js
+++ server/routes/trains.js
@@ -5,19 +5,23 @@ var xmlParser = require('fast-xml-parser')
 
 /* GET trains listing. */
 router.get('/:station_code/:direction/running', function(req, res, next) {
-  request('http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=' + req.params.station_code, (err, data) => {
-    let json = xmlParser.parse(data.body)
-    let trainsByDirection = []
-    if (json.ArrayOfObjStationData.hasOwnProperty('objStationData')) {
-      let data = Array.isArray(json.ArrayOfObjStationData.objStationData) ? json.ArrayOfObjStationData.objStationData : [json.ArrayOfObjStationData.objStationData]
+  request('http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode=' + req.params.station_code, (err, res, body) => {
+    if (res.statusCode == 200) {
+      let json = xmlParser.parse(body)
+      let trainsByDirection = []
+      if (json.ArrayOfObjStationData.hasOwnProperty('objStationData')) {
+        let data = Array.isArray(json.ArrayOfObjStationData.objStationData) ? json.ArrayOfObjStationData.objStationData : [json.ArrayOfObjStationData.objStationData]
 
-      // pick the trains with the right direction Southbound|Northbound
-      trainsByDirection = data.filter((x) => {
-        return x.Direction.toLowerCase() == req.params.direction
-      })
+        // pick the trains with the right direction Southbound|Northbound
+        trainsByDirection = data.filter((x) => {
+          return x.Direction.toLowerCase() == req.params.direction
+        })
+      }
+
+      res.send(trainsByDirection)
+    } else {
+      res.send(err)
     }
-
-    res.send(trainsByDirection)
   })
 })
 
